3f4b7eeb502467272af26ca0658872e5988ebd15
Kevin <kablaires@gmail.com>
2022-02-21 20:47:44 -0500

    Ajout de la fonctionnalité (côté frontend) qui permet de voir une liste de tous les quiz reliés à un Player.

--- projet-synthese-react/src/components/Player/AddNewQuiz.js
+++ projet-synthese-react/src/components/Player/AddNewQuiz.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useForm } from 'react-hook-form';
 
-const AddNewQuiz = () => {
+const AddNewQuiz = ({onAddQuiz}) => {
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = (quiz) => {
         quiz.player = JSON.parse(sessionStorage.getItem("user"))
-        addQuiz(quiz)
+        onAddQuiz(quiz)
             .then((data) => data.name !== undefined ? onCreationSucceded() : alert("Quiz name already exists, please try again"))
             .catch(() => alert("Quiz name already exists, please try again"));
     }
@@ -15,25 +15,13 @@ const AddNewQuiz = () => {
         alert("Quiz created succesfully!");
     }
 
-    const addQuiz = async (quiz) => {
-        const result = await fetch('http://localhost:8888/quiz/create_new_quiz',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(quiz)
-            })
-        return await result.json()
-    }
-
     return (
         <div>
             <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 + New quiz
             </button>
 
-            <div className="modal fade modal-dialog modal-dialog-centered" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
--- projet-synthese-react/src/components/Player/PlayerQuizzes.js
+++ projet-synthese-react/src/components/Player/PlayerQuizzes.js
@@ -1,9 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AddNewQuiz from './AddNewQuiz'
 import PlayerNavbar from './PlayerNavbar'
 
 const PlayerQuizzes = () => {
     const [quizzes, setQuizzes] = useState([])
+    const player = JSON.parse(sessionStorage.getItem("user"))
+
+    useEffect(() => {
+        const getQuizzes = async () => {
+            const quizzesFromServer = await fetchQuizzes(player.id)
+            setQuizzes(quizzesFromServer)
+        }
+        getQuizzes()
+    }, [])
+
+    const fetchQuizzes = async (quizId) => {
+        const res = await fetch(`http://localhost:8888/quiz/get-all-quizzes-by-player/${quizId}`)
+        return await res.json()
+    }
+
+    const addQuiz = async (quiz) => {
+        const result = await fetch('http://localhost:8888/quiz/create_new_quiz',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(quiz)
+            })
+        const data = await result.json()
+
+        setQuizzes(await fetchQuizzes(player.id))
+    }
 
     return (
         <div>
@@ -11,8 +39,18 @@ const PlayerQuizzes = () => {
             <div>
                 <h2 className='text-center mb-5'>My quizzes</h2>
                 <div className='mx-5 mb-2'>
-                    <AddNewQuiz />
+                    <AddNewQuiz onAddQuiz={addQuiz} />
                 </div>
+                {quizzes
+                    .map((quiz) => (
+                        <div key={quiz.idQuiz} className="card border-primary mt-3 mx-5 shadow">
+                            <div className="card-body">
+                                <h5 className="card-title">{quiz.name}</h5>
+                                <p className="card-text">Some quick</p>
+                            </div>
+                            <div className="card-footer border-primary">Footer</div>
+                        </div>
+                    ))}
             </div>
         </div>
     )
