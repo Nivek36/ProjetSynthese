3c3c637c86b7bbac250dc598edd05a5e52d36007
Kevin <kablaires@gmail.com>
2022-03-02 20:16:29 -0500

    Ajout de la fonctionnalité (côté frontend) qui permet à un Admin de voir la liste de quiz crée par lui.

--- projet-synthese-react/src/components/Admin/AdminQuizzes.js
+++ projet-synthese-react/src/components/Admin/AdminQuizzes.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AdminNavbar from './AdminNavbar'
 import AddNewQuiz from '../AddNewQuiz'
+import { useNavigate } from 'react-router-dom'
 
 const AdminQuizzes = () => {
+    const [quizzes, setQuizzes] = useState([])
     const admin = JSON.parse(sessionStorage.getItem("user"))
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const getQuizzes = async () => {
+            const quizzesFromServer = await fetchQuizzes(admin.id)
+            setQuizzes(quizzesFromServer)
+        }
+        getQuizzes()
+    }, [])
+
+    const fetchQuizzes = async (adminId) => {
+        const res = await fetch(`http://localhost:8888/quiz/get-all-quizzes-by-admin/${adminId}`)
+        return await res.json()
+    }
 
     const addQuiz = async (quiz) => {
         quiz.admin = admin
@@ -17,6 +33,8 @@ const AdminQuizzes = () => {
             })
         const data = await result.json()
 
+        setQuizzes(await fetchQuizzes(admin.id))
+
         return data;
     }
 
@@ -28,6 +46,18 @@ const AdminQuizzes = () => {
                 <div className='mx-5 mb-2'>
                     <AddNewQuiz onAddQuiz={addQuiz} />
                 </div>
+                {quizzes
+                    .map((quiz) => (
+                        <div key={quiz.idQuiz} className="card border-primary mt-3 mx-5 shadow">
+                            <div className="card-body">
+                                <h5 className="card-title">{quiz.name}</h5>
+                                <p className="card-text">Some quick description</p>
+                            </div>
+                            <div className="card-footer border-primary justify-content-end d-flex">
+                                <button className='btn btn-success btn-sm' onClick={e => { e.preventDefault(); navigate('/quiz', { state: quiz }) }}>+ Add questions</button>
+                            </div>
+                        </div>
+                    ))}
             </div>
         </div>
     )
