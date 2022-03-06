fe17a9b2ad5b90233a94bd9a67419a3d8dcdb596
Kevin <kablaires@gmail.com>
2022-03-05 22:01:45 -0500

    Ajout de la fonctionnalité (côté frontend) qui permet au joueur de voir la liste de tous les quiz publiés.

--- projet-synthese-react/src/components/HomePage/HomePage.js
+++ projet-synthese-react/src/components/HomePage/HomePage.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PlayerNavbar from '../Player/PlayerNavbar'
 
 const HomePage = () => {
-  return (
-    <PlayerNavbar />
-  )
+    const [publishedQuizzes, setPublishedQuizzes] = useState([])
+
+    useEffect(() => {
+        const getPublishedQuizzes = async () => {
+            const publishedQuizzesFromServer = await fetchPublishedQuizzes()
+            setPublishedQuizzes(publishedQuizzesFromServer)
+        }
+        getPublishedQuizzes()
+    }, [])
+
+    const fetchPublishedQuizzes = async () => {
+        const res = await fetch('http://localhost:8888/quiz/get-all-published-quizzes')
+        return await res.json()
+    }
+
+    return (
+        <div>
+            <PlayerNavbar />
+            <div>
+                <h2 className='text-center mb-5 text-color'>
+                    Find your favorite <span className='badge rounded-pill bg-primary'>Quiz</span> and <span className='badge rounded-pill bg-primary'>Play</span> !
+                </h2>
+                <div className='row'>
+                    {publishedQuizzes
+                        .map((quiz) => (
+                            <div key={quiz.idQuiz} className="col-4">
+                                <div className="card border-primary mt-3 mx-2 shadow">
+                                    <div className="card-body">
+                                        <h5 className="card-title text-center">{quiz.name}</h5>
+                                        <p className="card-text text-center">Some quick description</p>
+                                        <div className='d-flex justify-content-center'>
+                                            <button className='btn btn-primary'>Play !</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default HomePage
--- projet-synthese-react/src/components/Player/PlayerNavbar.js
+++ projet-synthese-react/src/components/Player/PlayerNavbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../../App.css'
 
 const PlayerNavbar = () => {
     const player = JSON.parse(sessionStorage.getItem("user"))
@@ -12,10 +13,11 @@ const PlayerNavbar = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="fas fa-bars"></span>
                     </button>
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div className="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
+                        <Link className='btn btn-primary mx-2' to="/HomePage">Home Page</Link>
                         <div className="dropdown">
-                            <button className="btn btn-primary dropdown-toggle mx-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                {player.username}
+                            <button className="btn btn-primary rounded-pill dropdown-toggle mx-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                {player.username + " "}
                             </button>
                             <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
                                 <li><Link className="dropdown-item" to="/player-quizzes">My quizzes</Link></li>
